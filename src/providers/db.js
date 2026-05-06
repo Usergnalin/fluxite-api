168a7b4f41be6854095802ed81be41aa2c11bef8
Usergnalin <usernilang@gmail.com>
2026-05-06 11:30:38 +0800

    Add SSL support for MySQL connection

--- src/providers/db.js
+++ src/providers/db.js
@@ -1,4 +1,7 @@
 import mysql from 'mysql2/promise'
+import logger from './logger.js'
+import fs from 'fs'
+import path from 'path'
 
 const setting = {
     connectionLimit: 100,
@@ -8,6 +11,21 @@ const setting = {
     database: process.env.MYSQL_DATABASE,
     multipleStatements: true,
     dateStrings: true,
+}
+
+if (process.env.MYSQL_USE_SSL === 'true') {
+    try {
+        const cert_path = path.join(process.cwd(), 'ca-certificate.crt')
+        setting.ssl = {
+            ca: fs.readFileSync(cert_path),
+        }
+        logger.info({}, 'MySQL SSL enabled')
+    } catch (error) {
+        logger.fatal(error, 'Failed to configure MySQL SSL')
+        throw error
+    }
+} else {
+    logger.warn('MySQL connecting without SSL')
 }
 
 const pool = mysql.createPool(setting)
