8434af42c4988489043cd578b10e768588208d92
Usergnalin <usernilang@gmail.com>
2026-05-06 13:11:46 +0800

    Add MYSQL_PORT to database connection settings

--- src/providers/db.js
+++ src/providers/db.js
@@ -6,6 +6,7 @@ import path from 'path'
 const setting = {
     connectionLimit: 100,
     host: process.env.MYSQL_HOST,
+    port: process.env.MYSQL_PORT,
     user: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
     database: process.env.MYSQL_DATABASE,
